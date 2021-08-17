ca9adc716883d9c3856825b7b88b4a5f1f5fa1ce
Adina Fakih <addy.fakih@gmail.com>
2021-08-17 14:34:40 +0500

    Custom Commands

---
+++ cypress/integration/Polkalokr/CROSS_SHAIN_BRIDGE.spec.js
@@ -0,0 +1,17 @@
+
+describe('TEST CROSS CHAIN BRIDGE',function()
+{
+    before(() => {
+        // runs once before all tests in the block
+        cy.fixture('example').then(function(data){
+            this.data=data
+        })
+    })
+
+    it('Cross Chain Bridge',function(){
+        cy.visit_Polkalokr()
+        //cy.Connect_Wallet_MetaMask()
+        cy.visit_Manage_Bridge('erc2')
+
+    })
+})
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,62 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+/// <reference types="cypress"/>
+
+Cypress.Commands.add('visit_Polkalokr', () => { 
+    //Website Address
+    cy.visit("http://35.178.147.68/")
+    //Verify URL of website
+    cy.url().should('include','35.178.147.68')
+    //Verify Title of website 
+    cy.title().should('be.equal','Polkalokr Migration') 
+})
+
+Cypress.Commands.add('Connect_Wallet_MetaMask', () => { 
+    //Click on Connect Wallet button
+    cy.get('.jss33').contains('Connect Wallet').click()
+    cy.wait(2000)
+    //Select MetaMask in modal
+    cy.get('.jss39').contains('Metamask').click()
+    //Enter user MetaMask password manually
+})
+
+// Cypress.Commands.add('verify_address', () => { 
+//     //Verify that when users changes account "From" textbox automatically updates to that account.
+//     var address = cy.get("jss32")
+//     cy.wait(2000)
+//     cy.log(address)
+// })
+
+Cypress.Commands.add('visit_Manage_Bridge',(Bridge_Name) => {
+    cy.get('.MuiTab-wrapper').contains('Manage Bridges').should('be.visible').trigger('mouseover')
+    // cy.get('.MuiTab-wrapper')
+    //   .find('div')
+    //   .should(($div) => {
+    //     if ($div.length !== 1) {
+    //         // you can throw your own errors
+    //         throw new Error('Did not find any element')
+    //     }
+    // })
+
+    // let bridge_links = '//*[@id="simple-menu"]/div[3]/ul/a'
+    // cy.xpath(bridge_links).its('length').then((len)=> {
+    //     cy.log('lENGTH IS ', len)
+    //     while (len !== 0){
+    //         cy.xpath(bridge_links).eq(len - 1).invoke('text').then((textt)=> {
+    //         cy.log(textt)
+    //         })
+    //     len--
+    //     }
+    // })
+
+    let bridge_links = '//*[@id="simple-menu"]/div[3]/ul/a'
+    cy.xpath(bridge_links).its('length').then((len)=> {
+        cy.log('lENGTH IS ', len)
+        while (len !== 0){
+            cy.xpath(bridge_links).eq(len - 1).invoke('text').then((textt)=> {
+            cy.log(textt)
+            if(textt === Bridge_Name){ 
+                cy.xpath(bridge_links).click() }
+            })
+        len--
+        }
+    })
+})
